c8e4a2e34863958b0a014f8bba6f32c6863356e5
mintzmolly <mintm016@newschool.edu>
2016-12-10 17:07:34 -0500

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,13 @@
+$("#yes").click(function(){
+
+      $("#answery").text("Make some eye contact sometime. You need to go on a walk, too. Don't you have carpal tunnel now? Get it together.");.show();
+  
+  } 
+
+  $(function() {
+
+  $("#no").click(function(){
+
+      $("#answern").text("Yes, you are. Don't be in denial. Get it together.");.show();
+    
+  } 
